4cba094bb86dbe75d7c47485eef39fe6a4d81761
Idan Goldman <idan@123code.co>
2022-04-27 13:57:21 +0300

    update commands and setup python

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    author = 'Idan Goldman',
-    author_email = 'idan@123code.co',
-    description = 'command line tool',
-    install_requires = ['pyyaml', 'toml', 'deepmerge'],
-    keywords = [''],
-    license = 'MIT',
-    name = 'cmd',
-    platform = 'any',
-    scripts = ['bin/cmd'],
-    url = 'https://github.com/123code-il/cmd',
-    version = '0.1.0',
-  )
+    author="Idan Goldman",
+    author_email="idan@123code.co",
+    description="command line tool",
+    install_requires=["pyyaml", "toml", "deepmerge"],
+    keywords=[""],
+    license="MIT",
+    name="cmd",
+    platform="any",
+    scripts=["bin/cmd"],
+    url="https://github.com/123code-il/cmd",
+    version="0.1.0",
+)
