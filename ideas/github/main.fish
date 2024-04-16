636c97a33a7bfc286c013348fd39b473ab0c5556
Idan Goldman <idan@goldman.work>
2024-04-17 01:58:13 +0300

    a bit more progress..

--- ideas/github/main.fish
+++ ideas/github/main.fish
@@ -1,21 +1,19 @@
 function main --argument-names action
     dependencies gh jq yq
-    sources
+    sources .
 
     set --local repo (verify_repo)
     set --local folder (exists_or_create folder .github/issues)
 
-    log_message INFO "$repo $folder"
-
-    # switch $action
-    #     case close reopen
-    #         # push_state $action $repo $folder
-    #     case update
-    #         # update_issues $repo $folder
-    #     case pull
-    #         pull_issues $repo $folder
-    #     case '*'
-    #         echo "Invalid action: $action"
-    #         return 1
-    # end
+    switch $action
+        case close reopen
+            # push_issues
+        case update
+            # update_issues
+        case pull
+            pull_issues
+        case '*'
+            echo "Invalid action: $action"
+            return 1
+    end
 end
--- ideas/github/pull_issues.fish
+++ ideas/github/pull_issues.fish
@@ -1,14 +1,14 @@
-function pull_issues --argument-names repo folder
+function pull_issues --no-scope-shadowing
     set --local issues (gh issue list --repo $repo --json number,title --jq '.[] | "\(.number)\t\(.title)"')
 
     for issue in $issues
         set issue_number (echo $issue | cut -f1)
-        set issue_title (echo $issue | cut -f2- | string replace -a '/' '_')
+        set issue_title (echo $issue | cut -f2- | string replace --regex --all --ignore-case "[^a-z0-9]+" "_")
 
-        set issue_data (pull_issue $issue_number)
+        # set issue_data (pull_issue $issue_number)
 
-        set filename "$folder/$issue_title.md"
-        echo $issue_data >$filename
-        echo "Downloaded issue #$issue_number $issue_title to $filename"
+        # set filename "$folder/$issue_title.md"
+        # echo $issue_data >$filename
+        # echo "Downloaded issue #$issue_number $issue_title to $filename"
     end
 end
