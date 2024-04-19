cc70741ee8a34a4bc4a47640ebe019ed2e362d1c
Idan Goldman <idan@goldman.work>
2024-04-19 14:38:06 +0300

    refactor variables to a bit more readable and updated log message script with CMDFILE_MANUALS_PATH path

--- conf.d/variables.fish
+++ conf.d/variables.fish
@@ -1,6 +1,7 @@
-set --global CMDFILE_COMPLETIONS_PATH (realpath (dirname (status --current-filename))/../completions)
-set --global CMDFILE_CONFD_PATH (realpath (dirname (status --current-filename)))
-set --global CMDFILE_FUNCTIONS_PATH (realpath (dirname (status --current-filename))/../functions)
-set --global CMDFILE_MANUALS_PATH (realpath (dirname (status --current-filename))/../manuals)
-set --global CMDFILE_PATH (realpath (dirname (status --current-filename))../)
-set --global CMDFILE_THEMES_PATH (realpath (dirname (status --current-filename))/../themes)
+
+set --global CMDFILE_PATH             (dirname (realpath (status --current-filename)/..))
+set --global CMDFILE_COMPLETIONS_PATH "$CMDFILE_PATH/completions"
+set --global CMDFILE_CONFD_PATH       "$CMDFILE_PATH/conf.d"
+set --global CMDFILE_FUNCTIONS_PATH   "$CMDFILE_PATH/functions"
+set --global CMDFILE_MANUALS_PATH     "$CMDFILE_PATH/manuals"
+set --global CMDFILE_THEMES_PATH      "$CMDFILE_PATH/themes"
--- functions/log_message.fish
+++ functions/log_message.fish
@@ -1,7 +1,7 @@
 # TODO: alias log_msg "log_message $argv"
 
 function log_message
-    set --local help_file_path (realpath (dirname (status --current-filename))/usage/log_message.txt)
+    set --local help_file_path "$CMDFILE_MANUALS_PATH/log_message.txt"
     set --local caller_basename (basename (status stack-trace | awk '/file/ {file=$NF} END {print file}'))
     set --local log_file_path $PWD/$caller_basename.log
 
