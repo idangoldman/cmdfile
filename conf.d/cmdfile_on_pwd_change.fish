83f149cb9f33017eaaad4ac1ed7e6c81a4ced4a2
Idan Goldman <idan@goldman.work>
2024-05-01 12:32:03 +0300

    update pythonpath global on pwn change and nix dev on flake.lock found

--- conf.d/cmdfile_on_pwd_change.fish
+++ conf.d/cmdfile_on_pwd_change.fish
@@ -1,3 +1,11 @@
 function on_pwd_change --on-variable PWD
     switch_language_version "node ruby"
+
+    if not contains -- (pwd -P) $PYTHONPATH
+        set --global --export --append PYTHONPATH (pwd -P)
+    end
+
+    if test -f $PWD/flake.lock
+        nix develop --impure
+    end
 end
