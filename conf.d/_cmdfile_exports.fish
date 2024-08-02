184facf6a5a94a989922ed7e208b9409883c579d
Idan Goldman <idan@goldman.work>
2024-08-02 16:57:12 +0300

    prepare for this to be a fisher plugin

---
+++ conf.d/_cmdfile_exports.fish
@@ -0,0 +1,7 @@
+set --global --export CMDFILE_PATH             (dirname (realpath (status --current-filename)/..))
+set --global --export CMDFILE_COMPLETIONS_PATH "$CMDFILE_PATH/completions"
+set --global --export CMDFILE_CONFD_PATH       "$CMDFILE_PATH/conf.d"
+set --global --export CMDFILE_FUNCTIONS_PATH   "$CMDFILE_PATH/functions"
+set --global --export CMDFILE_IDEAS_PATH       "$CMDFILE_PATH/__ideas"
+set --global --export CMDFILE_MANUALS_PATH     "$CMDFILE_PATH/manuals"
+set --global --export CMDFILE_THEMES_PATH      "$CMDFILE_PATH/themes"
---
+++ conf.d/cmdfile.fish
@@ -0,0 +1,11 @@
+function _cmdfile_install --on-event cmdfile_install
+    echo "Installing cmdfile..."
+end
+
+function _cmdfile_update --on-event cmdfile_update
+    echo "Updating cmdfile..."
+end
+
+function _cmdfile_uninstall --on-event cmdfile_uninstall
+    echo "Uninstalling cmdfile..."
+end
