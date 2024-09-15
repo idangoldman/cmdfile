7d9eb51428dc39f6b700d2e143f32d50ebb9e782
Idan Goldman <idan@goldman.work>
2024-09-15 14:44:44 +0300

    updated with a comment of todo in create folder function

--- functions/create_folder.fish
+++ functions/create_folder.fish
@@ -1,4 +1,9 @@
 function create_folder --description 'Create a folder if it does not exist' --argument-names folder
+
+    # TODO: Implement --follow (cd command) flag to change directory to the newly created folder
+    # argparse 'f/follow' -- $argv
+    # ex: create_folder --follow /path/to/folder
+
     if test -z $folder
         echo "Folder path is required"; and return 1
     end
